d13ec2f510318f79c481cec5aefae1bf8f9862c4
jhorgiGheovani <jhorgigheovani@gmail.com>
2023-11-15 18:56:07 +0700

    first commit

---
+++ app/src/main/java/com/eduproject/trafficsafetyeducation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eduproject.trafficsafetyeducation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
